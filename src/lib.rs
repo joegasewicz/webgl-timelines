882fc8cce5f2e10511816e41557735408c100bb5
joegasewicz <joegasewicz@gmail.com>
2022-07-29 17:55:43 +0100

    canvas

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use wasm_bindgen::prelude::*;
 use js_sys::*;
 use crate::timeline::TimeLine;
 
+
+
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
@@ -11,11 +13,8 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet(_name: &str) {
+    let element_id = String::from("webgl-timeline");
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have document on window");
-    let timeline =  TimeLine {
-        window,
-        document,
-    };
-    //
+    let timeline =  TimeLine::new(window, document, element_id);
 }
--- src/timeline.rs
+++ src/timeline.rs
@@ -1,15 +1,36 @@
 use wasm_bindgen::prelude::*;
-use js_sys::*;
-use web_sys::{Document, Window};
+use wasm_bindgen::JsCast;
+// use js_sys::*;
+use web_sys::{Document, Element, HtmlCanvasElement, HtmlDivElement, HtmlElement, Window};
 
 pub struct TimeLine {
     pub window: Window,
     pub document: Document,
+    pub element_id: String,
+    canvas: HtmlCanvasElement,
 }
 
 impl TimeLine {
-    fn create_canvas(&self) {
-        let body = self.document.body().expect("document should have a body");
+    pub fn new(window: Window, document: Document, element_id: String) -> Self {
+        let body = document.body().expect("document should have a body");
+        // Create the canvas element
+        let wrapper_el = document.get_element_by_id("webgl-timeline").unwrap();
+        let canvas_el = document.create_element("canvas").expect("should create canvas element");
+        canvas_el.set_attribute("id", "__webgl_timelines__").unwrap();
+        wrapper_el.append_with_node_1(&canvas_el).expect("should append node");
+        body.append_child(&wrapper_el).expect("sm");
+        // canvas
+        let canvas = document.get_element_by_id("__webgl_timelines__").unwrap();
+        let canvas: HtmlCanvasElement = canvas
+            .dyn_into::<HtmlCanvasElement>()
+            .map_err(|_| ())
+            .unwrap();
 
+        TimeLine{
+            window,
+            document,
+            element_id,
+            canvas,
+        }
     }
 }
